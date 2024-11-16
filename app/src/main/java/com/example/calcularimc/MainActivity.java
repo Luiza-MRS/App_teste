d5b9d256647bc74b45be205bcb1709e459f86887
ChrisCode-Dev <cristiansouza.dev@gmail.com>
2024-11-15 23:45:21 -0300

    Teste Chris

---
+++ app/src/main/java/com/example/calcularimc/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.calcularimc;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textResultado;
+    private EditText editPeso;
+    private EditText editTamanho;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+            textResultado = findViewById(R.id.textResultado);
+            editPeso = findViewById(R.id.edPeso);
+            editTamanho = findViewById(R.id.edAltura);
+
+    }
+
+    public void calcularImc(View view){
+
+        double peso = Double.parseDouble(editPeso.getText().toString());
+        double altura = Double.parseDouble(editTamanho.getText().toString());
+        double resultado = peso / (altura*altura);
+
+        //se
+        if(resultado<19){
+
+            textResultado.setText("Abaixo do peso");
+
+        }else if (resultado <= 19 || resultado < 25) {
+
+            textResultado.setText("Peso normal");
+            
+        } else if (resultado <= 25 || resultado < 30) {
+
+            textResultado.setText("Sobrepeso");
+            
+        }else if (resultado <= 30 || resultado < 40) {
+
+            textResultado.setText("obesidade tipo I");
+
+        }else if (resultado >= 40) {
+
+            textResultado.setText("obesidade tipo II");
+
+        }
+
+
+
+    }
+
+}
